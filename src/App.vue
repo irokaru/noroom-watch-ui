b05d0cbf950637332ffc7ab9bb5911c2819dc3e4
irokaru <karuta@nononotyaya.net>
2022-05-05 09:53:15 +0900

    add full screen setting

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,14 @@
 <template>
   <router-view />
 </template>
+
+<script lang="ts">
+import { Vue } from "vue-class-component";
+import { onFull } from "./utils/Screen";
+
+export default class App extends Vue {
+  created() {
+    document.body.addEventListener("click", (e) => onFull(document));
+  }
+}
+</script>
